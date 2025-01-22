fda8ba7dd4a37704792a71c5e48cedacb81d53b5
Jordan Cartwright <48060710+bpmxjordy@users.noreply.github.com>
2025-01-22 22:40:27 +0000

    Added user Login

--- App/App/urls.py
+++ App/App/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", user_view.register, name="register"),
+    path("login/", user_view.login, name="login"),
     path("index/", user_view.index, name="index"),
     path("register2/", user_view.register2, name="register2")
 ]
--- App/user/views.py
+++ App/user/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()  
-            return render(request, 'index.html', {"form": request.POST["username"]})
+            auth_login(request, form.save())
+            return render(request, 'index.html')
     else:
         form = UserCreationForm().as_p
     return render(request, 'register.html', {"form": form})
 
 def index(request):
     return render(request, "index.html")
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("/index/")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "login.html", {"form": form})
 
 def register2(request):
     if request.method == "POST":
